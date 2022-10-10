e1d5c086ec2e16b721482ee1dcc1ef00bb82f5be
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2022-10-10 20:39:26 +0200

    Update versioncheck.lua

--- server/versioncheck.lua
+++ server/versioncheck.lua
@@ -1,19 +1,20 @@
 Citizen.CreateThread( function()
-updatePath = "/Bluethefurry/FiveM-Resource-Version-Check-Thing" -- your git user/repo path
-resourceName = "FiveM-Resource-Version-Check-Thing ("..GetCurrentResourceName()..")" -- the resource name
+updatePath = "/NoroLexStudio/Fivem-SecuritySystem" -- your git user/repo path
+resourceName = "^1[security_system_scripts] ^0("..GetCurrentResourceName()..")" -- the resource name
 
 function checkVersion(err,responseText, headers)
 	curVersion = LoadResourceFile(GetCurrentResourceName(), "version") -- make sure the "version" file actually exists in your resource root!
 
 	if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
-		print("\n###############################")
-		print("\n"..resourceName.." is outdated, should be:\n"..responseText.."is:\n"..curVersion.."\nplease update it from https://github.com"..updatePath.."")
-		print("\n###############################")
+		print("\n^3—————————————————————————————————| Attention |————————————————————————————————")
+		print("\n"..resourceName.." is outdated. \nNewest Version: "..responseText.."\nYour Version: "..curVersion.."\nplease update it from https://github.com"..updatePath.."")
+		print("\n^3—————————————————————————————————| Attention |————————————————————————————————\n^0")
 	elseif tonumber(curVersion) > tonumber(responseText) then
 		print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update ( or downgrade? )")
 	else
-		print("\n"..resourceName.." is up to date, have fun!")
+		print("\n"..resourceName.." is up to date, have fun! \n")
 	end
+	
 end
 
 PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/master/version", checkVersion, "GET")
