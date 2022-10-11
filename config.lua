975eb14a4189d86f5b4746e8812f5a0e6084a79f
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2022-10-11 20:47:04 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -125,7 +125,7 @@ Config.NoClipTriggerCount = 3
 Config.KickForKeys = false
 
 Config.BlacklistedModels = {
-    "scarab",f
+    "scarab",
     "rhino",
     "jet",
     "stt_prop_stunt_track_uturn",
