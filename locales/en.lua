1c478e4ebffeb3711bfcf76466da43d4b861aee7
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2023-01-30 13:45:50 +0100

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,14 @@
+Locales['en'] = {
+
+  ['securitysys'] = 'securitysys',
+
+  --Messages
+  ['NoPermission'] = 'You do not have the required authorization for this',
+  ['combatloging'] = 'left the server while dead',
+  ['NoSteam'] = 'You don't have Steam open, please open Steam and then restart FiveM',
+
+  --dialog panelname
+  ['dpkick'] = 'kick player ID',
+  ['dpreson'] = 'reason',
+
+}
