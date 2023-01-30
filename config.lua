8fe48f0d27d7cc63479be541480189028cd685d6
NoroLexStudio <85202666+NoroLexStudio@users.noreply.github.com>
2023-01-30 13:48:26 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {}
 Config.Locale = 'en'
 
 Config.KickOnRestart = true --kick all players when securitysys restarting or stop
-Config.LeaveMarker = true --if this true then a marker spawns at the point where the player geleaved
+Config.LeaveMarker = true --if this true then a marker spawns at the point where the player leaved
 Config.LeaveMarkerID = 32
 Config.LeaveMarkerColor = {r = 255, g = 255, b = 255}
 Config.SteamKick = true --if this true then players whitout steam will kick from the server
